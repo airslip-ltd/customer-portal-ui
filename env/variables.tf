9d3160dd6ae99a1d15e55fad50896ab4786f7f1d
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-12-02 13:45:42 +0000

    Added initial environment

---
+++ env/variables.tf
@@ -0,0 +1,28 @@
+variable "short_environment" {
+  description = "The prefix used for all resources"
+}
+variable "location" {
+  description = "The Azure location where all resources should be created"
+}
+variable "environment" {
+  description = "The environment name being deployed to"
+}
+variable "web_tier" {
+  description = "The tier used for the app service plan"
+  default = "PremiumV2"
+}
+variable "web_size" {
+  description = "The size used for the app service plan"
+  default = "P1v2"
+}
+variable "admin_group_id" {}
+variable "certificate_path" {
+  default = "./Certificates/airslip.com.pfx"
+}
+variable "certificate_password" {}
+variable "certificate_name" {
+  default = "airslip-com-certificate"
+}
+variable "deployment_agent_group_id" {}
+variable "hostname" {}
+variable "certificate_thumbprint" {}
---
+++ env/variables/prod/backend.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "prod-tf-states"
+storage_account_name = "airslipprodtfstates"
+container_name       = "matching-states"
+key                  = "customer-portal-prod.terraform.tfstate"
