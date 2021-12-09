bde7a5eabea4db5b94d571e37f1cdec6150bc50d
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2021-12-09 15:13:18 +0000

    Added listing of banks

---
+++ src/src/components/_dashboard/account-list/index.js
@@ -0,0 +1,4 @@
+export { default as AccountListHead } from './AccountListHead';
+export { default as AccountListToolbar } from './AccountListToolbar';
+export { default as AccountMoreMenu } from './AccountMoreMenu';
+export { default as BankIcon } from './BankIcon';
---
+++ src/src/pages/accounts/AccountList.js
@@ -0,0 +1,196 @@
+import { filter } from 'lodash';
+import { useState, useEffect } from 'react';
+// material
+import {
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination
+} from '@mui/material';
+// redux
+import { useDispatch, useSelector } from '../../redux/store';
+import { getAccountList } from '../../redux/slices/accounts';
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// hooks
+import useSettings from '../../hooks/useSettings';
+// components
+import Page from '../../components/Page';
+import Scrollbar from '../../components/Scrollbar';
+import SearchNotFound from '../../components/SearchNotFound';
+import HeaderBreadcrumbs from '../../components/HeaderBreadcrumbs';
+import {
+  AccountListHead,
+  AccountListToolbar,
+  AccountMoreMenu,
+  BankIcon
+} from '../../components/_dashboard/account-list';
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'name', label: 'Name', alignRight: false },
+  { id: 'accountNumber', label: 'Account Number', alignRight: false },
+  { id: 'sortCode', label: 'Sort Code', alignRight: false },
+  { id: 'status', label: 'Status', alignRight: false },
+  { id: '' }
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
+export default function AccountList() {
+  const { themeStretch } = useSettings();
+  const dispatch = useDispatch();
+  const { accountList } = useSelector((state) => state.account);
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    dispatch(getAccountList());
+  }, [dispatch]);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - accountList.length) : 0;
+
+  const filteredUsers = applySortFilter(accountList, getComparator(order, orderBy), filterName);
+
+  const isUserNotFound = filteredUsers.length === 0;
+
+  return (
+    <Page title="Accounts | List | Airslip">
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <HeaderBreadcrumbs
+          heading="Account List"
+          links={[
+            { name: 'Dashboard', href: PATH_DASHBOARD.root },
+            { name: 'Accounts', href: PATH_DASHBOARD.partner.merchants.root },
+            { name: 'List' }
+          ]}
+        />
+
+        <Card>
+          <AccountListToolbar filterName={filterName} onFilterName={handleFilterByName} />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <AccountListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={accountList.length}
+                  onRequestSort={handleRequestSort}
+                />
+                <TableBody>
+                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                    const { id, name, status, bankId, accountNumber, sortCode } = row;
+
+                    return (
+                      <TableRow hover key={id} tabIndex={-1}>
+                        <TableCell component="th" scope="row" padding="none">
+                          <Stack direction="row" alignItems="center" spacing={2}>
+                            <BankIcon icon={bankId} />
+                            <Typography variant="subtitle2" noWrap>
+                              {name}
+                            </Typography>
+                          </Stack>
+                        </TableCell>
+                        <TableCell>{accountNumber}</TableCell>
+                        <TableCell>{sortCode}</TableCell>
+                        <TableCell>{status}</TableCell>
+                        <TableCell align="right">
+                          <AccountMoreMenu id={id} />
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={accountList.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
+    </Page>
+  );
+}
