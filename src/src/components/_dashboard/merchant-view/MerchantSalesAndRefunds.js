448891e062ecb5fafb499dd917f61357698e41b9
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-02 21:48:12 +0000

    Added revenue widget

--- src/src/components/_dashboard/merchant-view/MerchantSalesAndRefunds.js
+++ src/src/components/_dashboard/merchant-view/MerchantSalesAndRefunds.js
@@ -1,49 +1,39 @@
 import { merge } from 'lodash';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 // material
 import { Card, CardHeader, Box, TextField } from '@mui/material';
-//
 import { useTheme } from '@mui/material/styles';
+// redux
+import { useDispatch, useSelector } from '../../../redux/store';
+import { getRevenueByYear } from '../../../redux/slices/analytics';
+//
 
 import { BaseOptionChart } from '../../charts';
 
 // ----------------------------------------------------------------------
 
-const CHART_DATA = [
-  {
-    year: 2021,
-    data: [
-      {
-        name: 'Revenue',
-        data: [35789, 44678, 46753, 48998, 48967, 49877, 44546, 46388, 47890, 46777, 40124, 55678]
-      },
-      {
-        name: 'Refunds',
-        data: [3121.01, 4467.8, 3540.24, 5529.86, 4686.37, 3390.16, 4918.22, 5774.92, 3789, 6545.47, 7111.16, 6397.46]
-      }
-    ]
-  },
-  {
-    year: 2022,
-    data: [
-      {
-        name: 'Revenue',
-        data: [45789, 54678, 56753, 78998, 78967, 79877, 84546, 86388, 87890, 86777, 90124, 105678]
-      },
-      {
-        name: 'Refunds',
-        data: [4121.01, 5467.8, 4540.24, 5529.86, 8686.37, 6390.16, 5918.22, 7774.92, 8789, 9545.47, 8111.16, 7397.46]
-      }
-    ]
-  }
-];
+const YEARS = [2022, 2021];
 
 export default function MerchantSalesAndRefunds() {
-  const [seriesData, setSeriesData] = useState(2021);
+  const dispatch = useDispatch();
+  const [year, setYear] = useState(2022);
+  const { revenueStats } = useSelector((state) => state.analytics);
+  const [chartData, setChartData] = useState({});
 
-  const handleChangeSeriesData = (event) => {
-    setSeriesData(Number(event.target.value));
+  useEffect(() => {
+    dispatch(getRevenueByYear(year));
+  }, [dispatch, year]);
+
+  useEffect(() => {
+    if (!revenueStats.year) return;
+    console.log(revenueStats);
+
+    setChartData(revenueStats);
+  }, [revenueStats, setChartData]);
+
+  const handleChangeYear = (event) => {
+    setYear(Number(event.target.value));
   };
 
   const theme = useTheme();
@@ -64,14 +54,13 @@ export default function MerchantSalesAndRefunds() {
     <Card>
       <CardHeader
         title="Revenue and Refunds"
-        subheader="(+43%) than last year"
         action={
           <TextField
             select
             fullWidth
-            value={seriesData}
+            value={year}
             SelectProps={{ native: true }}
-            onChange={handleChangeSeriesData}
+            onChange={handleChangeYear}
             sx={{
               '& fieldset': { border: '0 !important' },
               '& select': { pl: 1, py: 0.5, pr: '24px !important', typography: 'subtitle2' },
@@ -79,22 +68,20 @@ export default function MerchantSalesAndRefunds() {
               '& .MuiNativeSelect-icon': { top: 4, right: 0, width: 20, height: 20 }
             }}
           >
-            {CHART_DATA.map((option) => (
-              <option key={option.year} value={option.year}>
-                {option.year}
+            {YEARS.map((option) => (
+              <option key={option} value={option}>
+                {option}
               </option>
             ))}
           </TextField>
         }
       />
 
-      {CHART_DATA.map((item) => (
-        <Box key={item.year} sx={{ mt: 3, mx: 3 }} dir="ltr">
-          {item.year === seriesData && (
-            <ReactApexChart type="line" series={item.data} options={chartOptions} height={364} />
-          )}
+      {chartData.series && (
+        <Box sx={{ mt: 3, mx: 3 }} dir="ltr">
+          <ReactApexChart type="line" series={chartData.series} options={chartOptions} height={364} />
         </Box>
-      ))}
+      )}
     </Card>
   );
 }
