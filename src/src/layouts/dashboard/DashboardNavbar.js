70776c17e1d79435c0ccb7a620259c17101bdf4c
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-06 20:03:00 +0000

    Tidied Navbar

--- src/src/layouts/dashboard/DashboardNavbar.js
+++ src/src/layouts/dashboard/DashboardNavbar.js
@@ -8,10 +8,7 @@ import { Box, Stack, AppBar, Toolbar, IconButton } from '@mui/material';
 import useCollapseDrawer from '../../hooks/useCollapseDrawer';
 // components
 import { MHidden } from '../../components/@material-extend';
-import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
-import LanguagePopover from './LanguagePopover';
-import NotificationsPopover from './NotificationsPopover';
 
 // ----------------------------------------------------------------------
 
@@ -63,12 +60,9 @@ export default function DashboardNavbar({ onOpenSidebar }) {
           </IconButton>
         </MHidden>
 
-        <Searchbar />
         <Box sx={{ flexGrow: 1 }} />
 
         <Stack direction="row" alignItems="center" spacing={{ xs: 0.5, sm: 1.5 }}>
-          <LanguagePopover />
-          <NotificationsPopover />
           <AccountPopover />
         </Stack>
       </ToolbarStyle>
