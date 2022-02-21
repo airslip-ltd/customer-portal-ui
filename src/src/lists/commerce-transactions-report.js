bb2891014f984280fc4b032f4baa1468346ba2d1
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-21 14:45:03 +0000

    Added commerce transaction report

---
+++ src/src/lists/commerce-transactions-report.js
@@ -0,0 +1,82 @@
+import { fCurrencyFromLong } from '../utils/formatNumber';
+import { fFullDateTime } from '../utils/formatDate';
+
+export const columns = [
+  {
+    field: 'id',
+    headerName: 'Id',
+    flex: 1,
+    searchable: false,
+    hide: true
+  },
+  {
+    field: 'source',
+    headerName: 'Source',
+    flex: 1
+  },
+  {
+    dataType: 'number',
+    field: 'total',
+    headerName: 'Total',
+    flex: 1,
+    valueFormatter: (params) => fCurrencyFromLong(params.value)
+  },
+  {
+    dataType: 'dateTime',
+    field: 'datetime',
+    headerName: 'Date',
+    flex: 1,
+    valueFormatter: (params) => fFullDateTime(params.value),
+    searchable: false
+  },
+  {
+    field: 'currencyCode',
+    headerName: 'Currency Code',
+    flex: 1
+  },
+  {
+    field: 'description',
+    headerName: 'Description',
+    flex: 1
+  },
+  {
+    field: 'isoFamilyCode',
+    headerName: 'Iso Family Code',
+    flex: 1,
+    hide: true
+  },
+  {
+    field: 'proprietaryCode',
+    headerName: 'Proprietary Code',
+    flex: 1,
+    hide: true
+  }
+];
+/**
+ public DataSources DataSource { get; set; }
+    public long TimeStamp { get; set; }
+    public string TrackingId { get; set; } = string.Empty;
+    public string? InternalId { get; init; }
+    public string? Source { get; init; }
+    public string? TransactionNumber { get; init; }
+    public string? RefundCode { get; init; }
+    public DateTime? Datetime { get; init; }
+    public string? StoreLocationId { get; init; }
+    public string? StoreAddress { get; init; }
+    public bool? OnlinePurchase { get; init; }
+    public long? Subtotal { get; init; }
+    public long? ServiceCharge { get; init; }
+    public long? Total { get; init; }
+    public string? CurrencyCode { get; init; }
+    public string? CustomerEmail { get; init; }
+    public string? OperatorName { get; init; }
+    public DateTime? Date { get; init; }
+    public string? Time { get; init; }
+    public string? Till { get; init; }
+    public string? Number { get; init; }
+    public string? Store { get; init; }
+    public string? Description { get; init; }
+    public int? Year { get; set; }
+    public int? Month { get; set; }
+    public int? Day { get; set; }
+ */
--- src/src/utils/formatDate.js
+++ src/src/utils/formatDate.js
@@ -5,5 +5,11 @@ import { format } from 'date-fns';
 export function fDateFromLong(number) {
   const t = new Date(Number(number));
 
-  return format(t, 'dd/MM/yyyy hh:mm:ss');
+  return format(t, 'dd/MM/yyyy hh:mm');
+}
+
+export function fFullDateTime(dateTime) {
+  const t = new Date(dateTime);
+
+  return format(t, 'dd/MM/yyyy hh:mm');
 }
