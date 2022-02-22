6dd235d3b483af91d35433d142ab4dde1e127f21
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-22 14:01:30 +0000

    Add demo data for merchant stats

--- src/src/components/_dashboard/merchant-view/MerchantRefunds.js
+++ src/src/components/_dashboard/merchant-view/MerchantRefunds.js
@@ -1,44 +1,24 @@
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-// redux
-import { useDispatch, useSelector } from '../../../redux/store';
-import { getRefundShapshot } from '../../../redux/slices/analytics';
 // utils
+import { useTheme } from '@mui/material/styles';
 import MerchantDashboardSnapshot from './MerchantDashboardSnapshot';
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
+// Demo data
+import { demoData } from '../../../utils/demo-data/MerchantRefunds';
 
 // ----------------------------------------------------------------------
 
-MerchantRefunds.propTypes = {
-  accountId: PropTypes.string
-};
-export default function MerchantRefunds({ accountId }) {
+export default function MerchantRefunds() {
   const theme = useTheme();
-  const dispatch = useDispatch();
-  const { refundStats } = useSelector((state) => state.analytics);
-  const [metricData, setMetricData] = useState({});
-  const [chartData, setChartData] = useState([]);
-  accountId = accountId || '';
 
-  useEffect(() => {
-    dispatch(getRefundShapshot(30, accountId));
-  }, [dispatch, accountId]);
-
-  useEffect(() => {
-    if (!refundStats[accountId] || !refundStats[accountId].metrics) return;
-    setMetricData(refundStats[accountId]);
-    const newData = refundStats[accountId].metrics.map((metric) => metric.balance);
-    setChartData(newData);
-  }, [refundStats, accountId, setChartData, setMetricData]);
+  const newData = demoData.metrics.map((metric) => metric.balance);
 
   return (
     <MerchantDashboardSnapshot
       title="Refunds"
-      metricData={metricData}
-      chartData={chartData}
+      metricData={demoData}
       navigateTo={PATH_DASHBOARD.analytics.commerceSummary}
+      chartData={newData}
       graphColor={theme.palette.secondary.main}
     />
   );
--- src/src/components/_dashboard/merchant-view/MerchantRevenue.js
+++ src/src/components/_dashboard/merchant-view/MerchantRevenue.js
@@ -1,41 +1,25 @@
-import { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-// redux
-import { useDispatch, useSelector } from '../../../redux/store';
-import { getSalesShapshot } from '../../../redux/slices/analytics';
 // utils
+import { useTheme } from '@mui/material/styles';
 import MerchantDashboardSnapshot from './MerchantDashboardSnapshot';
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
+// Demo data
+import { demoData } from '../../../utils/demo-data/MerchantRevenue';
 
 // ----------------------------------------------------------------------
-MerchantRevenue.propTypes = {
-  accountId: PropTypes.string
-};
-export default function MerchantRevenue({ accountId }) {
-  const dispatch = useDispatch();
-  const { salesStats } = useSelector((state) => state.analytics);
-  const [metricData, setMetricData] = useState({});
-  const [chartData, setChartData] = useState([]);
-  accountId = accountId || '';
 
-  useEffect(() => {
-    dispatch(getSalesShapshot(30, accountId));
-  }, [dispatch, accountId]);
+export default function MerchantRevenue() {
+  const theme = useTheme();
 
-  useEffect(() => {
-    if (!salesStats[accountId] || !salesStats[accountId].metrics) return;
-    setMetricData(salesStats[accountId]);
-    const newData = salesStats[accountId].metrics.map((metric) => metric.balance);
-    setChartData(newData);
-  }, [salesStats, accountId, setChartData, setMetricData]);
+  const newData = demoData.metrics.map((metric) => metric.balance);
 
   return (
     <MerchantDashboardSnapshot
       title="Revenue"
-      metricData={metricData}
+      metricData={demoData}
       navigateTo={PATH_DASHBOARD.analytics.commerceSummary}
-      chartData={chartData}
+      chartData={newData}
+      graphColor={theme.palette.secondary.main}
     />
   );
 }
