dc2accdf0e267a4dd1aae28984eb09efc852650e
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-23 14:11:40 +0000

    Fixed issue with dev usage

--- src/src/utils/feature-switch.js
+++ src/src/utils/feature-switch.js
@@ -1,17 +1,10 @@
 export function featureEnabled(featureName) {
   // Demo only features here...
-  if (process.env.REACT_APP_ENVIRONMENT !== 'demo') {
-    switch (featureName) {
-      case 'banking-recent-transactions':
-        return false;
-      case 'demo':
-        return false;
-      default:
-        return true;
-    }
-  }
-
   switch (featureName) {
+    case 'banking-recent-transactions':
+      return process.env.REACT_APP_ENVIRONMENT === 'demo';
+    case 'demo':
+      return process.env.REACT_APP_ENVIRONMENT === 'demo';
     case 'demo-to-complete':
       return false;
     default:
