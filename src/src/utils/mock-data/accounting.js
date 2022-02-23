d98e75286c11cb4d223fdbe77d70447b24ad6902
Tom McDonough <tmcdonough@airslip.com>
2022-02-23 08:59:30 +0000

    Added accounting logos

---
+++ src/src/utils/mock-data/accounting.js
@@ -0,0 +1,47 @@
+// ----------------------------------------------------------------------
+
+export const accountingProvider = [
+  'xero',
+  'quick-books-online',
+  'clear-books',
+  'exact',
+  'free-agent',
+  'fresh-books',
+  'kash-flow',
+  'kashoo',
+  'microsoft-dynamics-365',
+  'myob-account-right',
+  'myob-essentials',
+  'oracle-net-suite',
+  'pandle',
+  'quick-books-desktop',
+  'sage-200-cloud',
+  'sage-50',
+  'sage-business-accounting',
+  'sage-intacct',
+  'wave',
+  'zoho-books'
+];
+
+export const accountingProviderName = [
+  'Xero',
+  'QuickBooks Online',
+  'ClearBooks',
+  'Exact',
+  'FreeAgent',
+  'FreshBooks',
+  'KashFlow',
+  'Kashoo',
+  'Microsoft Dynamics 365',
+  'MYOB AccountRight',
+  'MYOB Essentials',
+  'Oracle NetSuite',
+  'Pandle',
+  'QuickBooks Desktop',
+  'Sage 200cloud',
+  'Sage 50',
+  'Sage Business Accounting',
+  'Sage Intacct',
+  'Wave',
+  'Zoho Books'
+];
