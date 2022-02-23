263d628c524d4aea31ad33a2537c94a0a32e511b
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-23 12:59:42 +0000

    Updated logos

--- src/src/pages/integrations/IntegrationLink.js
+++ src/src/pages/integrations/IntegrationLink.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 // material
 import { Card, Container, Grid, Stack, Typography, CardActionArea, CardContent } from '@mui/material';
 // redux
+import { isUndefined } from 'lodash';
 import { useDispatch, useSelector } from '../../redux/store';
 import { getProviders } from '../../redux/slices/providers';
 // routes
@@ -25,11 +27,32 @@ export default function IntegrationLink() {
     dispatch(getProviders());
   }, [dispatch]);
 
+  ProviderSelector.propTypes = {
+    provider: PropTypes.string.isRequired,
+    iconType: PropTypes.string
+  };
+
+  function ProviderSelector({ provider, iconType }) {
+    return (
+      <Grid item xs={6} md={4} align="center">
+        <Card sx={{ display: 'flex', alignItems: 'center' }} align="center">
+          <CardActionArea component={RouterLink} to={`${PATH_DASHBOARD.integrations.root}/${provider}/link`}>
+            <CardContent align="center">
+              <Stack style={{ margin: 'auto' }} spacing={2}>
+                <ProviderImage icon={provider} iconType={iconType} />
+              </Stack>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  }
+
   return (
-    <Page title="Commerce | Link | Airslip">
+    <Page title="Integrations | Link | Airslip">
       <Container maxWidth={themeStretch ? false : 'xl'}>
         <HeaderBreadcrumbs
-          heading="Connect Commerce"
+          heading="Connect Integration"
           links={[
             { name: 'Dashboard', href: PATH_DASHBOARD.root },
             { name: 'Commerce', href: PATH_DASHBOARD.integrations.root },
@@ -40,32 +63,39 @@ export default function IntegrationLink() {
         <Grid container spacing={3} justify="center">
           <Grid item xs={12}>
             <Typography noWrap variant="body" sx={{ color: 'text.secondary' }}>
-              Let's connect a commerce provider. Commerce data lets you visualise your sales data to help you access
-              more financial products.
+              Let's connect a commerce and accounting provider. Commerce data lets you visualise your sales data and
+              accounting data visualise to help you access more financial products.
             </Typography>
           </Grid>
+          {providers.hasData && (
+            <>
+              <Grid item xs={12}>
+                <Typography variant="h4">Commerce</Typography>
+              </Grid>
+              {providers.response.results
+                .filter((row) => !isUndefined(row.posProvider) && row.posProvider != null)
+                .map((row) => {
+                  const { posProvider } = row;
+                  return <ProviderSelector key={posProvider} provider={posProvider} />;
+                })}
 
-          {providers.hasData &&
-            providers.response.results.map((row) => {
-              const { posProvider } = row;
-
-              return (
-                <Grid key={posProvider} item xs={6} md={4} align="center">
-                  <Card sx={{ display: 'flex', alignItems: 'center' }} align="center">
-                    <CardActionArea
-                      component={RouterLink}
-                      to={`${PATH_DASHBOARD.integrations.root}/${posProvider}/link`}
-                    >
-                      <CardContent align="center">
-                        <Stack style={{ margin: 'auto' }} spacing={2}>
-                          <ProviderImage icon={posProvider} />
-                        </Stack>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              );
-            })}
+              <Grid item xs={12}>
+                <Typography variant="h4">Accounting</Typography>
+              </Grid>
+              {providers.response.results
+                .filter((row) => !isUndefined(row.accountingProvider) && row.accountingProvider != null)
+                .map((row) => {
+                  const { accountingProvider } = row;
+                  return (
+                    <ProviderSelector
+                      key={accountingProvider}
+                      provider={accountingProvider}
+                      iconType="accounting_logos"
+                    />
+                  );
+                })}
+            </>
+          )}
         </Grid>
       </Container>
     </Page>
