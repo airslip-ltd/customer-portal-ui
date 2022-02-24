82480d418afb86c935845866ba8e7c02a7853ccb
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-24 13:38:09 +0000

    Updated to use new frontdoor implementation

--- env/main.tf
+++ env/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "2.90.0"
     }
   }
@@ -18,20 +18,19 @@ locals {
   tags = {
     Environment = "${var.environment}"
   }
-  app_id = "customer-portal-ui"
-  app_id_short = "cprtl"
-  short_environment = var.short_environment
-  location = var.location
-  app_tier = var.web_tier
-  app_size = var.web_size
-  health_check_path = ""
-  admin_group_id = var.admin_group_id
-  certificate_name = var.certificate_name
-  certificate_path = var.certificate_path
-  certificate_password = var.certificate_password
+  app_id                    = "customer-portal-ui"
+  app_id_short              = "cprtl"
+  short_environment         = var.short_environment
+  location                  = var.location
+  app_tier                  = var.web_tier
+  app_size                  = var.web_size
+  health_check_path         = ""
+  admin_group_id            = var.admin_group_id
+  certificate_name          = var.certificate_name
+  certificate_path          = var.certificate_path
+  certificate_password      = var.certificate_password
   deployment_agent_group_id = var.deployment_agent_group_id
-  hostname = var.hostname
-  certificate_thumbprint = var.certificate_thumbprint
+  hostname                  = var.hostname
 }
 
 data "azurerm_client_config" "current" {}
@@ -40,21 +39,20 @@ module "web_app" {
   source = "./tf_modules/Airslip.Terraform.Modules/recipes/node_js_web_app"
 
   app_configuration = {
-    app_id = local.app_id,
-    hostname = local.hostname,
-    app_id_short = local.app_id_short,
-    short_environment = local.short_environment,
-    location = local.location,
-    tags = local.tags,
-    app_tier = local.app_tier,
-    app_size = local.app_size,
-    health_check_path = local.health_check_path, 
-    certificate_name = local.certificate_name, 
-    certificate_path = local.certificate_path, 
+    app_id               = local.app_id,
+    hostname             = local.hostname,
+    app_id_short         = local.app_id_short,
+    short_environment    = local.short_environment,
+    location             = local.location,
+    tags                 = local.tags,
+    app_tier             = local.app_tier,
+    app_size             = local.app_size,
+    health_check_path    = local.health_check_path,
+    certificate_name     = local.certificate_name,
+    certificate_path     = local.certificate_path,
     certificate_password = local.certificate_password,
-    tenant_id = data.azurerm_client_config.current.tenant_id,
-    admin_group_id = local.admin_group_id,
-    deployer_id = local.deployment_agent_group_id,
-    certificate_thumbprint = local.certificate_thumbprint
+    tenant_id            = data.azurerm_client_config.current.tenant_id,
+    admin_group_id       = local.admin_group_id,
+    deployer_id          = local.deployment_agent_group_id
   }
 }
