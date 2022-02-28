60e3eb9c5e04d838d6b7f21bbcb3a85256c3baad
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-02-28 16:56:32 +0000

    Added basics for search

--- src/src/components/_dashboard/account-list/index.js
+++ src/src/components/_dashboard/account-list/index.js
@@ -1,5 +1,4 @@
 export { default as AccountListHead } from './AccountListHead';
-export { default as AccountListToolbar } from './AccountListToolbar';
 export { default as AccountMoreMenu } from './AccountMoreMenu';
 export { default as BankIcon } from './BankIcon';
 export { default as BankImage } from './BankImage';
