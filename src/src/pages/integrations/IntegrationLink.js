22249280050e71b183283db0ebb89886787f6056
Graham Whitehouse <83427700+gwairslip@users.noreply.github.com>
2022-03-01 09:03:29 +0000

    Updated display for integration linking

--- src/src/pages/integrations/IntegrationLink.js
+++ src/src/pages/integrations/IntegrationLink.js
@@ -2,7 +2,22 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
 // material
-import { Card, Container, Grid, Stack, Typography, CardActionArea, CardContent } from '@mui/material';
+import {
+  Card,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  CardActionArea,
+  CardContent,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  FormControl,
+  FormLabel
+} from '@mui/material';
+
+import { textAlign } from '@mui/system';
 import SearchBox from '../../components/_common/SearchBox';
 // redux
 import { useDispatch, useSelector } from '../../redux/store';
@@ -23,6 +38,12 @@ export default function IntegrationLink() {
   const dispatch = useDispatch();
   const { providers } = useSelector((state) => state.provider);
   const [filterBy, setFilterBy] = useState('');
+  const [integrationFilters, setIntegrationFilters] = useState({
+    banking: true,
+    commerce: true,
+    accounting: true
+  });
+  const { banking, commerce, accounting } = integrationFilters;
 
   useEffect(() => {
     dispatch(getProviders());
@@ -31,6 +52,13 @@ export default function IntegrationLink() {
   const onFilterChanged = (event) => {
     const { value } = event.target;
     setFilterBy(value);
+  };
+
+  const handleChange = (event) => {
+    setIntegrationFilters({
+      ...integrationFilters,
+      [event.target.name]: event.target.checked
+    });
   };
 
   ProviderSelector.propTypes = {
@@ -42,21 +70,52 @@ export default function IntegrationLink() {
     imageType: PropTypes.string
   };
 
+  function CheckboxLabels() {
+    return (
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel component="legend">Show Integrations</FormLabel>
+        <FormGroup aria-label="position" row>
+          <FormControlLabel
+            control={<Checkbox checked={banking} onChange={handleChange} name="banking" />}
+            label="Banking"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={commerce} onChange={handleChange} name="commerce" />}
+            label="Commerce"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={accounting} onChange={handleChange} name="accounting" />}
+            label="Accounting"
+          />
+        </FormGroup>
+      </FormControl>
+    );
+  }
+
   function ProviderSelector({ provider, integration, integrationType, imageType, friendlyName, installationCount }) {
     return (
-      <Grid item xs={6} md={4} align="center">
-        <Card sx={{ display: 'flex', alignItems: 'center' }} align="center">
+      <Grid item xs={6} md={4}>
+        <Card sx={{ display: 'flex', minHeight: 150 }}>
           <CardActionArea component={RouterLink} to={`${PATH_INTEGRATE.authorise}/${provider}/${integration}`}>
-            <CardContent align="center">
-              <Stack style={{ margin: 'auto' }} spacing={2}>
-                <ProviderImage icon={integration} integrationType={integrationType} imageType={imageType} />
-                <Typography variant="subtitle1">{friendlyName}</Typography>
-                {installationCount > 0 && (
-                  <>
-                    <Typography variant="body2">Installed {installationCount} times in the last 7 days</Typography>
-                  </>
-                )}
-              </Stack>
+            <CardContent>
+              <Grid container spacing={1}>
+                <Grid item xs={6} sx={{ minHeight: 60 }}>
+                  <ProviderImage icon={integration} integrationType={integrationType} imageType={imageType} />
+                </Grid>
+                <Grid item xs={6} sx={{ textAlign: 'right' }}>
+                  {installationCount > 0 && (
+                    <>
+                      <Typography variant="body2">
+                        <Typography variant="subtitle1">{installationCount} installs</Typography>
+                        in the last 7 days
+                      </Typography>
+                    </>
+                  )}
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="subtitle1">{friendlyName}</Typography>
+                </Grid>
+              </Grid>
             </CardContent>
           </CardActionArea>
         </Card>
@@ -70,30 +129,34 @@ export default function IntegrationLink() {
   };
 
   function ProviderList({ integrationType, imageType }) {
+    if (!integrationFilters[integrationType.toLowerCase()]) return <></>;
+
+    const list = providers.response.results.filter((row) => {
+      const str = `${row.name}# ${row.friendlyName}# ${row.id}`;
+      return row.integrationType === integrationType && str.match(filterBy);
+    });
+
+    if (list.length === 0) return <></>;
+
     return (
       <>
         <Grid item xs={12}>
           <Typography variant="h4">{integrationType}</Typography>
         </Grid>
-        {providers.response.results
-          .filter((row) => {
-            const str = `${row.name}# ${row.friendlyName}# ${row.id}`;
-            return row.integrationType === integrationType && str.match(filterBy);
-          })
-          .map((row) => {
-            const { id, integrationType, provider, friendlyName, installationCount } = row;
-            return (
-              <ProviderSelector
-                key={id}
-                provider={provider}
-                integration={id}
-                integrationType={integrationType}
-                imageType={imageType}
-                friendlyName={friendlyName}
-                installationCount={installationCount}
-              />
-            );
-          })}
+        {list.map((row) => {
+          const { id, integrationType, provider, friendlyName, installationCount } = row;
+          return (
+            <ProviderSelector
+              key={id}
+              provider={provider}
+              integration={id}
+              integrationType={integrationType}
+              imageType={imageType}
+              friendlyName={friendlyName}
+              installationCount={installationCount}
+            />
+          );
+        })}
       </>
     );
   }
@@ -117,12 +180,16 @@ export default function IntegrationLink() {
               you can visualise your data to make improved decisions and get access to more financial products.
             </Typography>
           </Grid>
-          <Grid item xs={4}>
-            <SearchBox placeholder="Find your integration" filterName={filterBy} onFilterName={onFilterChanged} />
-          </Grid>
 
           {providers.hasData && (
             <>
+              <Grid item xs={12}>
+                <Stack direction="row" spacing={2}>
+                  <SearchBox placeholder="Find your integration" filterName={filterBy} onFilterName={onFilterChanged} />
+
+                  <CheckboxLabels />
+                </Stack>
+              </Grid>
               <ProviderList integrationType="Banking" imageType="svg" />
               <ProviderList integrationType="Commerce" />
               <ProviderList integrationType="Accounting" />
