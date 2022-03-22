49f155a1a49e5552161dd2eb7227a52422b396b0
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-22 20:42:26 +0000

    Now uses live values

--- src/src/components/_dashboard/merchant-view/MerchantBalance.js
+++ src/src/components/_dashboard/merchant-view/MerchantBalance.js
@@ -1,24 +1,40 @@
+import { useEffect, useState } from 'react';
 // utils
 import { useTheme } from '@mui/material/styles';
 import MerchantDashboardSnapshot from './MerchantDashboardSnapshot';
+// material
+// redux
+import { useDispatch, useSelector } from '../../../redux/store';
+import { getCurrentBalance } from '../../../redux/slices/analytics';
 // routes
 import { PATH_DASHBOARD } from '../../../routes/paths';
-// Demo data
-import { demoData } from '../../../utils/demo-data/MerchantBalances';
 
 // ----------------------------------------------------------------------
 
 export default function MerchantBalance() {
   const theme = useTheme();
+  const dispatch = useDispatch();
+  const { currentBalance } = useSelector((state) => state.analytics);
+  const [metricData, setMetricData] = useState({});
+  const [chartData, setChartData] = useState([]);
 
-  const newData = demoData.metrics.map((metric) => metric.balance);
+  useEffect(() => {
+    dispatch(getCurrentBalance());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!currentBalance.metrics) return;
+    setMetricData(currentBalance);
+    const newData = currentBalance.metrics.map((metric) => metric.balance);
+    setChartData(newData);
+  }, [currentBalance, setChartData, setMetricData]);
 
   return (
     <MerchantDashboardSnapshot
       title="Cash in bank"
-      metricData={demoData}
+      metricData={metricData}
       navigateTo={PATH_DASHBOARD.analytics.accountBalances}
-      chartData={newData}
+      chartData={chartData}
       graphColor={theme.palette.chart.red[0]}
     />
   );
