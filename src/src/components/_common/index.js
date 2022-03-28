775b72d0aa5216280c6c0fcfc6027891d13dedfd
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-28 15:17:18 +0100

    More ui updates

--- src/src/components/_common/index.js
+++ src/src/components/_common/index.js
@@ -2,3 +2,5 @@ export { default as FormSection } from './FormSection';
 export { default as SearchBox } from './SearchBox';
 export { default as HelpDialogue } from './HelpDialogue';
 export { default as SuccessDialogue } from './SuccessDialogue';
+export { default as HelpSection } from './HelpSection';
+export { default as HelpCard } from './HelpCard';
--- src/src/pages/setup/SetupComplete.js
+++ src/src/pages/setup/SetupComplete.js
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // material
-import { Grid, Button, Typography } from '@mui/material';
+import { Grid, Button, Typography, Stack, Card, Box, CardHeader, CardContent } from '@mui/material';
+import IntegrationIcon from '../../components/_dashboard/account-list/IntegrationIcon';
+import { HelpCard, HelpSection } from '../../components/_common';
+// redux
+import { useDispatch, useSelector } from '../../redux/store';
+import { search as integrationSearch } from '../../redux/slices/integration';
+import { GET_ALL_QUERY } from '../../redux/common/constants';
 // layouts
 import OnboardingLayout from '../../layouts/OnboardingLayout';
 // hooks
@@ -14,9 +20,15 @@ import { PATH_DASHBOARD } from '../../routes/paths';
 
 export default function SetupComplete() {
   const { memberDetails } = useAuth();
+  const dispatch = useDispatch();
   const { onCompleteSetup } = useMemberDetails();
   const [serviceCount, setServiceCount] = useState(0);
+  const { integration } = useSelector((state) => state.integration);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(integrationSearch(GET_ALL_QUERY));
+  }, [dispatch]);
 
   useEffect(() => {
     if (!memberDetails) return;
@@ -44,22 +56,51 @@ export default function SetupComplete() {
         )
       }
     >
-      <Grid container spacing={3}>
-        {memberDetails.linkedServices.map((row) => {
-          const { integrationCount, integrationType } = row;
+      <Grid container spacing={3} sx={{ mt: 2 }}>
+        <Grid item md={8}>
+          <Card>
+            <CardHeader title="Services Linked" />
+            <CardContent>
+              <Stack spacing={2}>
+                {integration.hasData &&
+                  integration.response.results.map((row) => {
+                    const { id, integrationProviderId, provider, name, accountDetail } = row;
+                    const { friendlyName, integrationType } = provider;
 
-          return (
-            <Grid item xs={12} key={integrationType}>
-              <Typography variant="body2">
-                You added{' '}
-                <strong>
-                  {integrationCount} {integrationType}
-                </strong>{' '}
-                integrations
-              </Typography>
-            </Grid>
-          );
-        })}
+                    return (
+                      <Box key={id} sx={{ display: 'flex' }}>
+                        <Box>
+                          <IntegrationIcon icon={integrationProviderId} />
+                        </Box>
+                        <Box sx={{ flexGrow: 1 }}>
+                          <Stack sx={{ ml: 2 }} spacing={1}>
+                            <Typography variant="subtitle2">{friendlyName}</Typography>
+                            {integrationType === 'Commerce' && <Typography variant="body2">{name}</Typography>}
+                            {integrationType === 'Banking' && (
+                              <>
+                                <Typography variant="body2">
+                                  Account number: {accountDetail.accountNumber} - Sort code: {accountDetail.sortCode}
+                                </Typography>
+                              </>
+                            )}
+                          </Stack>
+                        </Box>
+                      </Box>
+                    );
+                  })}
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item sx={{ display: { xs: 'none', md: 'block' } }} md={4}>
+          <HelpCard>
+            <Stack spacing={4}>
+              <HelpSection title="Some Help">
+                <Typography variant="body2">Here we will put some help</Typography>
+              </HelpSection>
+            </Stack>
+          </HelpCard>
+        </Grid>
       </Grid>
     </OnboardingLayout>
   );
