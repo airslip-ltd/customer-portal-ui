6c37992265ecce300f91169d8cb6cf305d4a2f9e
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-28 09:46:50 +0100

    Removed demo data from partner home

--- src/src/utils/feature-switch.js
+++ src/src/utils/feature-switch.js
@@ -2,8 +2,9 @@ export function featureEnabled(featureName) {
   // Demo only features here...
   switch (featureName) {
     case 'banking-recent-transactions':
-      return process.env.REACT_APP_ENVIRONMENT === 'demo';
+    case 'partner-risk-focus':
     case 'industry-exposure':
+    case 'connected-businesses':
       return process.env.REACT_APP_ENVIRONMENT === 'demo';
     case 'demo':
       return process.env.REACT_APP_ENVIRONMENT === 'demo';
