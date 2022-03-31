a8f2aeb3a3247b3f92920d803512cbee72ed5e84
Graham Whitehouse <gwhitehouse@airslip.com>
2022-03-31 09:37:32 +0100

    Update to include date picker

--- src/src/components/_dashboard/merchant-view/MerchantSummary.js
+++ src/src/components/_dashboard/merchant-view/MerchantSummary.js
@@ -1,233 +1,39 @@
 // material
-import { Grid, Typography } from '@mui/material';
-// utils
-import { featureEnabled } from '../../../utils/feature-switch';
-// hooks
-import useDataOwner from '../../../hooks/useDataOwner';
+import { Grid } from '@mui/material';
 import { DateSelectionProvider } from '../../../contexts';
 // components
-import {
-  MerchantRevenue,
-  MerchantRefunds,
-  MerchantBalance,
-  CashInflowOutflow,
-  RevenueGrowthBenchmarking,
-  NewVsReturningCustomers,
-  DebtServiceCoverageRatio,
-  LeverageRatio,
-  DebtRatio,
-  DebtToCapitalRatio,
-  TotalDebtToTotalAssetsRatio,
-  OperatingMargin,
-  GrossProfitMargin,
-  CommonOutgoings,
-  LargestCustomersByRevenue,
-  ActiveVsChurnCustomers,
-  RefundRateVsBenchmark,
-  AccountsReceivables,
-  AccountsPayables,
-  AverageDebtorDays,
-  AverageCreditorDays,
-  LateInvoicesReceived,
-  LateBillsPaid,
-  RevenueForecasts,
-  BankingExpensesCategories,
-  Customers,
-  CashflowByAccount,
-  MerchantSalesAndRefunds
-} from '.';
+import { MerchantRevenue, MerchantRefunds, MerchantBalance, CashflowByAccount, MerchantSalesAndRefunds } from '.';
 
 // ----------------------------------------------------------------------
 
 export default function MerchantSummary() {
-  const { ownerEntityId } = useDataOwner();
-  // ownerEntityId = ownerEntityId ?? 'my-account-4';
-
   return (
-    <Grid container spacing={3}>
-      {featureEnabled('demo') && (
-        <>
-          <Grid item xs={12}>
-            <Typography variant="h4">Leverage</Typography>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <DebtServiceCoverageRatio accountId={ownerEntityId} />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <DebtRatio accountId={ownerEntityId} />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <DebtToCapitalRatio accountId={ownerEntityId} />
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography variant="h4">Liquidity</Typography>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <CashInflowOutflow accountId="my-account-1" />
-          </Grid>
-
-          {ownerEntityId === 'my-account-1' && featureEnabled('demo') && (
-            <Grid item xs={12} md={4}>
-              <Customers />
-            </Grid>
-          )}
-        </>
-      )}
-
-      {featureEnabled('demo') && (
-        <Grid item xs={12} md={4}>
-          <RevenueGrowthBenchmarking accountId="my-account-1" />
-        </Grid>
-      )}
-
-      {featureEnabled('demo') && (
+    <>
+      <Grid container spacing={3}>
         <Grid item xs={12} md={4}>
           <MerchantRevenue />
         </Grid>
-      )}
 
-      <Grid item xs={12} md={4}>
-        <MerchantRevenue />
-      </Grid>
+        <Grid item xs={12} md={4}>
+          <MerchantRefunds />
+        </Grid>
 
-      <Grid item xs={12} md={4}>
-        <MerchantRefunds />
-      </Grid>
-
-      <Grid item xs={12} md={4}>
-        <MerchantBalance />
+        <Grid item xs={12} md={4}>
+          <MerchantBalance />
+        </Grid>
       </Grid>
 
       <DateSelectionProvider>
-        <Grid item xs={12} md={6}>
-          <CashflowByAccount />
-        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <CashflowByAccount />
+          </Grid>
 
-        <Grid item xs={12} md={6}>
-          <MerchantSalesAndRefunds />
+          <Grid item xs={12} md={6}>
+            <MerchantSalesAndRefunds />
+          </Grid>
         </Grid>
       </DateSelectionProvider>
-
-      {/* <Grid item xs={12} md={12}>
-          <BankingRecentTransactions accountId={accountId} />
-        </Grid> */}
-
-      {/* <Grid item xs={12}>
-          <MerchantSalesAndRefunds />
-        </Grid>
-        <Grid item xs={12}>
-          <MerchantCashflow />
-        </Grid> */}
-
-      {featureEnabled('demo') && (
-        <>
-          <Grid item xs={12}>
-            <Typography variant="h4">Profitability</Typography>
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <OperatingMargin accountId="my-account-1" />
-          </Grid>
-        </>
-      )}
-      {featureEnabled('demo') && (
-        <Grid item xs={12} md={4}>
-          <GrossProfitMargin accountId="my-account-1" />
-        </Grid>
-      )}
-
-      {featureEnabled('demo') && (
-        <>
-          <Grid item xs={12}>
-            <Typography variant="h4">Growth Analytics</Typography>
-          </Grid>
-
-          <Grid item xs={12} md={4}>
-            <NewVsReturningCustomers accountId="my-account-1" />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <ActiveVsChurnCustomers accountId="my-account-1" />
-          </Grid>
-        </>
-      )}
-      {featureEnabled('demo') && (
-        <>
-          <Grid item xs={12}>
-            <Typography variant="h4">Outgoings</Typography>
-          </Grid>
-          <Grid item xs={8}>
-            <BankingExpensesCategories />
-          </Grid>
-        </>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <LeverageRatio accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <TotalDebtToTotalAssetsRatio accountId="my-account-1" />
-        </Grid>
-      )}
-
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <CommonOutgoings accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <LargestCustomersByRevenue accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <ActiveVsChurnCustomers accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <RefundRateVsBenchmark accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <AccountsReceivables accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <AccountsPayables accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <AverageDebtorDays accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <AverageCreditorDays accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <LateInvoicesReceived accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={4}>
-          <LateBillsPaid accountId="my-account-1" />
-        </Grid>
-      )}
-      {featureEnabled('demo-to-complete') && (
-        <Grid item xs={12} md={8}>
-          <RevenueForecasts accountId="my-account-1" />
-        </Grid>
-      )}
-    </Grid>
+    </>
   );
 }
--- src/src/contexts/DateSelectionContext.js
+++ src/src/contexts/DateSelectionContext.js
@@ -1,5 +1,16 @@
+import { Box, Button, Stack, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
-import { createContext, useState } from 'react';
+import React, { createContext, useState } from 'react';
+import Picker from 'react-month-picker';
+
+MonthBox.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+function MonthBox({ value, onClick }) {
+  return <Button onClick={onClick}>{value}</Button>;
+}
 
 // ----------------------------------------------------------------------
 
@@ -16,21 +27,68 @@ DateSelectionProvider.propTypes = {
 };
 
 function DateSelectionProvider({ children }) {
-  const [startDate, setStartDate] = useState('2021-03-01');
-  const [endDate, setEndDate] = useState('2022-03-01');
+  const [startDateRaw, setStartDate] = useState({ year: 2021, month: 3 });
+  const [endDateRaw, setEndDate] = useState({ year: 2022, month: 3 });
+  const startMonthRef = React.createRef();
+  const endMonthRef = React.createRef();
+
+  const pickerLang = {
+    months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    from: 'From',
+    to: 'To'
+  };
+
+  const makeText = (m) => {
+    if (m && m.year && m.month) return `${pickerLang.months[m.month - 1]}. ${m.year}`;
+    return '?';
+  };
 
   return (
     <DateSelectionContext.Provider
       value={{
-        startDate,
-        endDate,
         dateQuery: {
-          startDate,
-          endDate
+          startDate: `${startDateRaw.year}-${startDateRaw.month}-01`,
+          endDate: `${endDateRaw.year}-${endDateRaw.month}-01`
         }
       }}
     >
-      {children}
+      <Stack>
+        <Box sx={{ display: 'flex' }}>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box>
+            <Stack direction="row" sx={{ py: 2 }}>
+              <Typography variant="subtitle1" sx={{ mt: 0.8, mr: 1 }}>
+                Date range
+              </Typography>
+              <Picker
+                ref={startMonthRef}
+                autoRange
+                value={startDateRaw}
+                lang={pickerLang.months}
+                onChange={(year, month) => setStartDate({ year, month })}
+                onDismiss={setStartDate}
+              >
+                <MonthBox value={`From: ${makeText(startDateRaw)}`} onClick={() => startMonthRef.current.show()} />
+              </Picker>
+              <Typography variant="subtitle1" sx={{ mt: 0.8, mx: 1 }}>
+                -
+              </Typography>
+              <Picker
+                ref={endMonthRef}
+                autoRange
+                value={endDateRaw}
+                lang={pickerLang.months}
+                onChange={(year, month) => setEndDate({ year, month })}
+                onDismiss={setEndDate}
+              >
+                <MonthBox value={`To: ${makeText(endDateRaw)}`} onClick={() => endMonthRef.current.show()} />
+              </Picker>
+            </Stack>
+          </Box>
+          <Box sx={{ flexGrow: 1 }} />
+        </Box>
+        <>{children}</>
+      </Stack>
     </DateSelectionContext.Provider>
   );
 }
